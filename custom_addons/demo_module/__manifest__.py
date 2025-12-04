7f6e23ee84b958308ed03270c23edb5dcd14a2e4
Rabinder <rabinder@nicerx.com>
2025-12-04 13:56:54 +0530

    Initial project structure with demo module

---
+++ custom_addons/demo_module/__manifest__.py
@@ -0,0 +1,11 @@
+# __manifest__.py
+{
+    "name": "Demo Module",
+    "version": "16.0.1.0.0",
+    "author": "You",
+    "category": "Tools",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
